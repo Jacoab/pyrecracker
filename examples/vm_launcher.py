24ecc9678866e64bad056f1d743e80a5efe898f1
jacoab <jjongewaard1@gmail.com>
2026-03-14 15:30:31 +0000

    Add high level VM wrapper

--- examples/vm_launcher.py
+++ examples/vm_launcher.py
@@ -1,96 +1,43 @@
 import signal
 from time import sleep
+import argparse
 
-from pyrecracker.host_env import HostEnvironment
-from pyrecracker.client import FirecrackerClient
-from pyrecracker.client_types import (
-    BootSource,
-    Drive,
-    MachineConfiguration,
-    InstanceActionInfo,
-    NetworkInterface,
-)
-
-
-SOCKET_PATH = "/tmp/firecracker.sock"
-API = "http+unix://%2Ftmp%2Ffirecracker.sock"
-
-BASE_ROOTFS = "./rootfs.ext4"
-WORK_ROOTFS = "../firecracker/ubuntu-24.04.ext4"
-KERNEL = "../firecracker/vmlinux-6.1.155"
-
-TAP_DEV = "tap0"
-HOST_IP = "172.16.0.1"
-GUEST_IP = "172.16.0.2"
+from pyrecracker.vm import VMManager
 
 
 def main():
-    # Create a host environment and Firecracker client
-    host_env = HostEnvironment()
-    
-    # Setup the TAP device for networking
-    host_env.add_tap_device(TAP_DEV)
-    host_env.add_tap_address(HOST_IP, TAP_DEV)
-    host_env.set_tap_up(TAP_DEV)
-    host_env.exec()
+    parser = argparse.ArgumentParser(description="Launch a Firecracker VM with custom rootfs and kernel.")
+    parser.add_argument('--base-rootfs', type=str, help='Path to the base rootfs image')
+    parser.add_argument('--work-rootfs', type=str, help='Path to the working rootfs image')
+    parser.add_argument('--kernel', type=str, default="../firecracker/vmlinux-6.1.155", help='Path to the kernel image')
+    args = parser.parse_args()
 
-    client = FirecrackerClient(SOCKET_PATH)
+    vm = VMManager("/tmp/firecracker.sock", args.kernel)
+    vm.vcpu_count = 1
+    vm.mem_size_mib = 512
+    vm.boot_args = "console=ttyS0 reboot=k panic=1 pci=off"
+    vm.drive_id = "rootfs"
+    vm.is_root_device = True
+    vm.path_on_host = args.work_rootfs
+    vm.is_read_only = False
+    vm.iface_id = "eth0"
+    vm.host_dev_name = "tap0"
+    vm.guest_mac = "AA:FC:00:00:00:01"
+    vm.host_ip = "172.16.0.1"
+    vm.guest_ip = "172.16.0.2"
 
-    #
-    # CONFIGURING THE VM
-    #
-
-    # Define the machine configuration
-    machine_config = MachineConfiguration(
-        vcpu_count=1,
-        mem_size_mib=512,
-    )
-    client.put_machine_config(machine_config)
-
-    # Define the boot source
-    boot_source = BootSource(
-        kernel_image_path=KERNEL,
-        boot_args="console=ttyS0 reboot=k panic=1 pci=off"
-    )
-    client.put_boot_source(boot_source)
-
-    # Define the drive
-    drive = Drive(
-        drive_id="rootfs",
-        is_root_device=True,
-        path_on_host=WORK_ROOTFS,
-        is_read_only=False,
-    )
-    client.put_drives(drive)
-
-    network_interface = NetworkInterface(
-        iface_id="eth0",
-        host_dev_name=TAP_DEV,
-        guest_mac="AA:FC:00:00:00:01",
-    )
-    client.put_network_interfaces(network_interface)
-
-    #
-    # Start the microVM
-    #
-    action_info = InstanceActionInfo(action_type="InstanceStart")
-    client.put_actions(action_info)
+    vm.configure()
+    vm.start()
 
     print("VM launched successfully!")
 
     def sigterm_handler(signum, frame):
         print("SIGINT received, stopping the VM...")
-        action_info = InstanceActionInfo(action_type="SendCtrlAltDel")
-        client.put_actions(action_info)
+        vm.stop()
         print("VM stopped successfully!")
-        print("Cleaning up host environment...")
-        sleep(5)
-        host_env.rm(SOCKET_PATH)
-        host_env.del_tap_device(TAP_DEV)
-        host_env.exec()
         print("Exiting.")
         exit(0)
-        
+
     signal.signal(signal.SIGINT, sigterm_handler)
     while True:
         pass
---
+++ src/pyrecracker/vm.py
@@ -0,0 +1,328 @@
+from typing import Optional
+from time import sleep
+
+from pyrecracker.client import FirecrackerClient
+from pyrecracker.client_types import (
+    MachineConfiguration, 
+    BootSource, 
+    Drive, 
+    InstanceActionInfo,
+    NetworkInterface
+)
+from pyrecracker.host_env import HostEnvironment
+
+
+class VMManager:
+    """
+    Class for managing a Firecracker MicroVM instance.  All settings for 
+    Firecracker VMs that are normally set via the Firecracker API are exposed 
+    as properties of this class.  Methods for controlling a VM's lifecycle 
+    are also provided.
+
+    Attributes:
+        __host_env: HostEnvironment - Host environment manager
+        __client: FirecrackerClient - Client for interacting with the Firecracker API
+        __boot_source: BootSource - Boot source configuration for the VM
+        __machine_config: MachineConfiguration - Machine configuration for the VM
+        __drive: Drive - Drive configuration for the VM
+        __network_interface: NetworkInterface - Network interface configuration for the VM
+        __host_ip: Optional[str] - Host IP address for the network interface
+        __guest_ip: Optional[str] - Guest IP address for the network interface
+    """
+    def __init__(self, socket_path: str, kernel_image_path: str) -> None:
+        self.__host_env = HostEnvironment()
+        self.__client = FirecrackerClient(socket_path)
+        
+        self.__boot_source = BootSource(
+            kernel_image_path=kernel_image_path,
+            boot_args="console=ttyS0 reboot=k panic=1 pci=off"
+        )
+        self.__machine_config = MachineConfiguration(
+            vcpu_count=1, 
+            mem_size_mib=128
+        )
+        self.__drive: Drive = Drive(drive_id="rootfs", is_root_device=True)
+        self.__network_interface = NetworkInterface(iface_id="eth0", host_dev_name="tap0")
+
+        self.__host_ip: Optional[str] = None
+        self.__guest_ip: Optional[str] = None
+
+    @property
+    def socket_path(self) -> str:
+        """
+        Socket path for the Firecracker API.
+        """
+        return self.__client.socket_path
+
+    @property
+    def kernel_image_path(self) -> str:
+        """
+        Get or set the path to the kernel image for the VM.
+        """
+        return self.__boot_source.kernel_image_path
+
+    @kernel_image_path.setter
+    def kernel_image_path(self, kernel_image_path: str) -> None:
+        self.__boot_source.kernel_image_path = kernel_image_path
+
+    @property
+    def boot_args(self) -> Optional[str]:
+        """
+        Get or set the boot arguments for the VM.
+        """
+        return self.__boot_source.boot_args
+
+    @boot_args.setter
+    def boot_args(self, boot_args: str) -> None:
+        self.__boot_source.boot_args = boot_args
+
+    @property
+    def initrd_path(self) -> Optional[str]:
+        """
+        Path to the initrd image for the VM.
+        """
+        return self.__boot_source.initrd_path
+
+    @initrd_path.setter
+    def initrd_path(self, initrd_path: str) -> None:
+        self.__boot_source.initrd_path = initrd_path
+
+    @property
+    def mem_size_mib(self) -> int:
+        """
+        Memory size in MiB for the VM.
+        """
+        return self.__machine_config.mem_size_mib
+
+    @mem_size_mib.setter
+    def mem_size_mib(self, mem_size_mib: int) -> None:
+        self.__machine_config.mem_size_mib = mem_size_mib
+
+    @property
+    def vcpu_count(self) -> int:
+        """
+        Number of vCPUs for the VM.
+        """
+        return self.__machine_config.vcpu_count
+
+    @vcpu_count.setter
+    def vcpu_count(self, vcpu_count: int) -> None:
+        self.__machine_config.vcpu_count = vcpu_count
+
+    @property
+    def smt(self) -> Optional[bool]:
+        """
+        Simultaneous multithreading (SMT) for the VM.
+        """
+        return self.__machine_config.smt
+
+    @smt.setter
+    def smt(self, smt: bool) -> None:
+        self.__machine_config.smt = smt
+
+    @property
+    def track_dirty_pages(self) -> Optional[bool]:
+        """
+        Dirty page tracking for the VM.
+        """
+        return self.__machine_config.track_dirty_pages
+
+    @track_dirty_pages.setter
+    def track_dirty_pages(self, track_dirty_pages:bool) -> None:
+        self.__machine_config.track_dirty_pages = track_dirty_pages
+
+    @property
+    def huge_pages(self) -> Optional[str]:
+        """
+        Huge pages setting for the VM.
+        """
+        return self.__machine_config.huge_pages
+
+    @huge_pages.setter
+    def huge_pages(self, huge_pages: str) -> None:
+        self.__machine_config.huge_pages = huge_pages
+
+    @property
+    def drive_id(self) -> str:
+        """
+        Drive ID for the VM's root device.
+        """
+        return self.__drive.drive_id
+
+    @drive_id.setter
+    def drive_id(self, drive_id: str) -> None:
+        self.__drive.drive_id = drive_id
+
+    @property
+    def is_root_device(self) -> bool:
+        """
+        Whether the drive is the root device.
+        """
+        return self.__drive.is_root_device
+
+    @is_root_device.setter
+    def is_root_device(self, is_root_device: bool) -> None:
+        self.__drive.is_root_device = is_root_device
+
+    @property
+    def partuuid(self) -> Optional[str]:
+        """
+        Partition UUID for the drive.
+        """
+        return self.__drive.partuuid
+
+    @partuuid.setter
+    def partuuid(self, partuuid: str) -> None:
+        self.__drive.partuuid = partuuid
+
+    @property
+    def cache_type(self) -> Optional[str]:
+        """
+        Cache type for the drive.
+        """
+        return self.__drive.cache_type
+
+    @cache_type.setter
+    def cache_type(self, cache_type: str) -> None:
+        self.__drive.cache_type = cache_type
+
+    @property
+    def is_read_only(self) -> Optional[bool]:
+        """
+        Whether the drive is read-only.
+        """
+        return self.__drive.is_read_only
+
+    @is_read_only.setter
+    def is_read_only(self, is_read_only: bool) -> None:
+        self.__drive.is_read_only = is_read_only
+
+    @property
+    def path_on_host(self) -> Optional[str]:
+        """
+        Path to the drive on the host.
+        """
+        return self.__drive.path_on_host
+
+    @path_on_host.setter
+    def path_on_host(self, path_on_host: str) -> None:
+        self.__drive.path_on_host = path_on_host
+
+    @property
+    def io_engine(self) -> Optional[str]:
+        """
+        IO engine for the drive.
+        """
+        return self.__drive.io_engine
+
+    @io_engine.setter
+    def io_engine(self, io_engine: str) -> None:
+        self.__drive.io_engine = io_engine
+
+    @property
+    def socket(self) -> Optional[str]:
+        """
+        Socket path for the drive.
+        """
+        return self.__drive.socket
+
+    @socket.setter
+    def socket(self, socket: str) -> None:
+        self.__drive.socket = socket
+
+    @property
+    def host_dev_name(self) -> str:
+        """
+        Host device name for the network interface.
+        """
+        return self.__network_interface.host_dev_name
+
+    @host_dev_name.setter
+    def host_dev_name(self, host_dev_name: str) -> None:
+        self.__network_interface.host_dev_name = host_dev_name
+
+    @property
+    def iface_id(self) -> str:
+        """
+        Network interface ID for the VM.
+        """
+        return self.__network_interface.iface_id
+
+    @iface_id.setter
+    def iface_id(self, iface_id: str) -> None:
+        self.__network_interface.iface_id = iface_id
+
+    @property
+    def guest_mac(self) -> Optional[str]:
+        """
+        Guest MAC address for the network interface.
+        """
+        return self.__network_interface.guest_mac
+
+    @guest_mac.setter
+    def guest_mac(self, guest_mac: str) -> None:
+        self.__network_interface.guest_mac = guest_mac
+
+    @property
+    def host_ip(self) -> Optional[str]:
+        """
+        Host IP address for the network interface.
+        """
+        return self.__host_ip
+    
+    @host_ip.setter
+    def host_ip(self, host_ip: str) -> None:
+        self.__host_ip = host_ip
+
+    @property
+    def guest_ip(self) -> Optional[str]:
+        """
+        Guest IP address for the network interface.
+        """
+        return self.__guest_ip
+    
+    @guest_ip.setter
+    def guest_ip(self, guest_ip: str) -> None:
+        self.__guest_ip = guest_ip
+
+    def configure(self) -> None:
+        """
+        Configure the VM with the current settings.  This will 
+        do the following:
+        - set up the TAP device for networking if host and guest IPs are provided
+        - set the machine configuration
+        - set the boot source
+        - set the drive configuration
+        - set the network interface configuration
+        """
+
+        # Set up the TAP device for networking if host and guest IPs are provided
+        if self.host_ip is not None and self.guest_ip is not None:
+            self.__host_env.add_tap_device(self.host_dev_name)
+            self.__host_env.add_tap_address(self.host_ip, self.host_dev_name)
+            self.__host_env.set_tap_up(self.host_dev_name)
+            self.__host_env.exec()
+
+        # Configure the VM with the specified settings
+        self.__client.put_machine_config(self.__machine_config)
+        self.__client.put_boot_source(self.__boot_source)
+        self.__client.put_drives(self.__drive)
+        self.__client.put_network_interfaces(self.__network_interface)
+
+    def start(self) -> None:
+        """
+        Starts the microVM.
+        """
+        action_info = InstanceActionInfo(action_type="InstanceStart")
+        self.__client.put_actions(action_info)
+
+    def stop(self) -> None:
+        """
+        Stops the microVM.
+        """
+        action_info = InstanceActionInfo(action_type="SendCtrlAltDel")
+        self.__client.put_actions(action_info)
+
+        self.__host_env.rm(self.socket_path)
+        self.__host_env.del_tap_device(self.host_dev_name)
+        self.__host_env.exec()
